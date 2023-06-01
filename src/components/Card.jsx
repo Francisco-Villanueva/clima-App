572234cf02b3895484e303305c37ea1fa2b9a19d
Francisco Villanueva <franciscovillanuevaj99@gmail.com>
2023-06-01 06:49:16 -0300

    idk

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,32 +1,34 @@
-import React from 'react'
-import './Card2.css'
-import DaysCard from './DaysCard'
-export default function Card({name, country, forecast, current}) {
-
+import React from "react";
+import "./Card2.css";
+import DaysCard from "./DaysCard";
+export default function Card({ name, country, forecast, current }) {
   // console.log('CARD => ', name, country, forecast, current)
   return (
-    <div className='card-container'>
-      <div className='top-card'>
-        <div className='left-top'>
+    <div className="card-container">
+      <div className="top-card">
+        <div className="left-top">
           <img src={current.condition.icon} alt="" />
-          <div className='extra-data'>
-          <b>{current.temp_c} °C</b>
+          <div className="extra-data">
+            <b>{current.temp_c} °C</b>
             <p>Humedad: {current.humidity}</p>
             <p>Viento a {current.wind_kph} km/h</p>
             <p>Precipitaciones {current.precip_in}%</p>
           </div>
         </div>
-        <div className='right-top'>
+        <div className="right-top">
           <b>{name}</b>
           <p>{country}</p>
-          <p className='fechaHoy'>{current.last_updated}</p>
-          <p className='fechaHoy'>{current.condition.text}</p>
+          <p className="fechaHoy">{current.last_updated}</p>
+          <p className="fechaHoy">{current.condition.text}</p>
         </div>
       </div>
-      <div className='bottom-card'>
-        
-        {forecast.map(e=>(<button className='btn-dayCard'><DaysCard data={e}/></button>))}
+      <div className="bottom-card">
+        {forecast.map((e) => (
+          <button className="btn-dayCard">
+            <DaysCard data={e} />
+          </button>
+        ))}
       </div>
     </div>
-  )
+  );
 }
